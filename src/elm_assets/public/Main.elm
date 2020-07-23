990e4a7f02a9d20fdc1c1c88e23254a1c6c59f50
chenyan-dfinity <yan.chen@dfinity.org>
2020-07-22 22:45:33 -0700

    sketch out candid binding

--- src/elm_assets/public/Main.elm
+++ src/elm_assets/public/Main.elm
@@ -11,17 +11,27 @@ main =
   Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
 port sendMessage : (String, List E.Value) -> Cmd msg
-port messageReceiver : ((String, List E.Value) -> msg) -> Sub msg
+port messageReceiver : ((String, E.Value) -> msg) -> Sub msg
+port messageError : ((String, E.Value) -> msg) -> Sub msg
+
+greet : String -> Cmd msg
+greet name = sendMessage("greet", [E.string name])
+greetDecoder : E.Value -> String
+greetDecoder val = Result.withDefault "" (D.decodeValue D.string val)
+fib : Int -> Cmd msg                   
+fib n = sendMessage("fib", [E.int n])
+fibDecoder : E.Value -> Int
+fibDecoder val = Result.withDefault -1 (D.decodeValue D.int val)
 
 subscriptions : Model -> Sub Msg
-subscriptions _ = messageReceiver Recv
+subscriptions _ = Sub.batch [messageReceiver Recv, messageError Error]
 
 type alias Model = { input : String, output : String }
 
 init : () -> (Model, Cmd msg)
 init _ = (Model "" "", Cmd.none)
 
-type Msg = Send | Recv (String, List E.Value) | Changed String
+type Msg = Send | Recv (String, E.Value) | Changed String | Error (String, E.Value)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -31,11 +41,20 @@ update msg model =
         ( { model | output = "Waiting..." }
         --, sendMessage ("getCaller", [])
         --, sendMessage ("fib", [E.int (Maybe.withDefault 0 (String.toInt model.input))])
-        , sendMessage ("greet", [E.string model.input])
+        --, sendMessage ("greet", [E.string model.input])
+        --, greet(model.input)
+        , fib(Maybe.withDefault 0 (String.toInt model.input))
         )
     Recv (method, message) ->
-        let result = String.join ", " (List.map (\v -> E.encode 0 v) message) in
+        --let result = String.join ", " (List.map (\v -> E.encode 0 v) message) in
+        --let result = String.fromInt (fibDecoder message) in
+        let result = E.encode 0 message in
         ( { model | output = method ++ ": " ++ result }
+        , Cmd.none
+        )
+    Error (method, message) ->
+        let error = E.encode 0 message in
+        ( { model | output = method ++ ": " ++ error }
         , Cmd.none
         )
 
--- src/elm_assets/public/index.js
+++ src/elm_assets/public/index.js
@@ -1,30 +1,17 @@
 import backend from 'ic:canisters/backend';
-import candid from 'ic:idl/backend';
-import { IDL } from '@dfinity/agent';
 import { Elm } from './Main.elm';
-
-const service = Object.assign(...candid({IDL})._fields.map(([key, val]) => ({[key]: val})));
-
-function normalizeReturn(method, res) {
-  const func = service[method];
-  let result;
-  if (func.retTypes.length === 0) {
-    result = [];
-  } else if (func.retTypes.length === 1) {
-    result = [res];
-  } else {
-    result = res;
-  }
-  return result;
-}
 
 const app = Elm.Main.init({
   node: document.getElementById('app'),
 });
 
 app.ports.sendMessage.subscribe(([method, args]) => {
-  backend[method](...args).then(res => {
-    const result = normalizeReturn(method, res);
-    app.ports.messageReceiver.send([method, result]);
-  });
+  backend[method](...args)
+    .then(res => {
+      app.ports.messageReceiver.send([method, res]);
+    })
+    .catch(error => {
+      app.ports.messageError.send([method, error.message]);
+      throw error;
+    });
 });
